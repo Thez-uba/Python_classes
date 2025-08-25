532a1bfa8afa1ba571bf4af22c769d20215be90a
Thelma Uba <uba.thelmad@gmail.com>
2025-08-25 20:24:53 +0100

    second

--- pracBankAcct.py
+++ pracBankAcct.py
@@ -31,30 +31,32 @@ class User:
 
 # Customer Class
 class Customer(User):
-    def __init__(self, firstname, lastname, email, phone, gender, nin, home_address, occupation, state_of_origin, lga, dob):
-        super().__init__(firstname, lastname, email, phone, gender)
-        self.nin = nin
-        self.home_address = home_address
-        self.occupation = occupation
-        self.state_of_origin = state_of_origin
-        self.lga = lga
-        self.dob = dob
+    def __init__(self):
+        pass
+        # super().__init__(firstname, lastname, email, phone, gender)
+        # self.nin = nin
+        # self.home_address = home_address
+        # self.occupation = occupation
+        # self.state_of_origin = state_of_origin
+        # self.lga = lga
+        # self.dob = dob
     
-    def registerCustomer(self, account_number):
+    def registerCustomer(self, firstname, lastname, email, phone, gender, nin, home_address, occupation, state_of_origin, lga, dob, account_number):
         new_customer = {
-            "firstname" : self.name,
-            "lastname" : self.lastname,
-            "email" : self.name,
-            "phone_number": self.phone,
-            "gender" : self.gender,
-            "nin" : self.nin,
-            "home_address" : self.home_address,
-            "occupation" : self.occupation,
-            "state_of_origin" : self.state_of_origin,
-            "dob" : self.dob,
-            "lga" : self.lga,
-            "account_number" : self.account_number,
-            "balance" : 0.00
+            "firstname" : firstname,
+            "lastname" : lastname,
+            "email" : email,
+            "phone_number": phone,
+            "gender" : gender,
+            "nin" : nin,
+            "home_address" : home_address,
+            "occupation" : occupation,
+            "state_of_origin" : state_of_origin,
+            "dob" : dob,
+            "lga" : lga,
+            "account_number" : account_number,
+            "balance" : 0.00,
+            "restricted" : False
         }
         customers.append(new_customer)
         print(f"Registration was successful, Your account number is: {account_number}")
@@ -180,6 +182,17 @@ class Staff(User):
         staff.append(new_staff)
         return print(f"{staff_name} added succesfully,\n Email: {email}, Password: {password}")
     
+    # Deposit money for cusotmer
+    def depositForCustomer(self, staff_id, password):
+         for staff_user in staff:
+            if staff_user["id"] == staff_id and staff_user["password"] == password:
+                customer_account = input("Enter customer account number: ")
+                amount = input("Enter amount: ")
+                customer = Customer()
+                customer.depositMoney(customer_account, amount)
+            elif staff_user["id"] != staff_id and staff_user["password"] != password and staff [-1] == staff_user:
+                print("Incorrect login credentials")
+    
     # Create account for customer
     def createAccountForCustomer(self, staff_id, password):
         for staff_user in staff:
@@ -187,19 +200,19 @@ class Staff(User):
                 customer = Customer()
                 firstname = input("Enter customer firstame: ")
                 lastname = input("Enter customer lastname: ")
-                email = input("Enter coustomer email: ")
+                email = input("Enter customer email: ")
                 phone = input("Enter customer phone number: ")
                 nin = input("Enter customer NIN: ")
                 home = input("Enter customer home address: ")
                 gender = input("Enter customer gender: ")
                 dob = input("Enter customer DOB: ")
-                state = input("Enter custoer state odf origin: ")
+                state = input("Enter customer state of origin: ")
                 lga = input("Enter customer LGA: ")
                 occupation = input("Enter customer occupuation: ")
                 account_number = input("Enter 10 digit account number: ")
-                customer.resgisterCustomer(account_number, firstname, lastname, email, phone, nin, home, gender, dob, state, lga, occupation)
+                customer.registerCustomer(firstname, lastname, email, phone, nin, home, gender, dob, state, lga, occupation, account_number)
             elif staff_user["id"] != staff_id and staff_user["password"] != password and staff [-1] == staff_user:
-                print("Incorrect login credentisls")
+                print("Incorrect login credentials")
 
     # Method for staff to handle transfer
     def transfer(self, staff_id, password):
@@ -207,8 +220,9 @@ class Staff(User):
             if staff_user["id"] == staff_id and staff_user["password"] == password:
                 sender_account = input("Enter sender account number: ")
                 receiver_account = input("Enter receiver account number: ")
+                amount = input("Enter amount: ")
                 customer = Customer()
-                customer.transferMoney(sender_account, receiver_account)
+                customer.transferMoney(sender_account, receiver_account, amount)
             elif staff_user["id"] != staff_id and staff_user["password"] != password and staff [-1] == staff_user:
                 print("Incorrect login credentials")
 
@@ -244,7 +258,7 @@ class Staff(User):
 
 class Manager:
     # Create staff as a manager
-    def createStaff(username, password):
+    def createStaff(self, username, password):
         if username == manager["username"] and password == manager["password"]:
             new_staff = Staff()
             staff_name = input("Enter staff name: ")
@@ -258,7 +272,7 @@ class Manager:
             print("Incorrect login credentials")
 
     # Method for manager to restrict customer account
-    def restrict_customer_access(username, password):
+    def restrict_customer_access(self, username, password):
         if username == manager["username"] and password == manager["password"]:
             customer = Customer()
             account_number = input("Enter the customer account to be restrcted: ")
@@ -267,7 +281,7 @@ class Manager:
             print("Incorrect login credentials")
             
     # Method for manager to delete customers account
-    def delete_customer_account(username, password):
+    def delete_customer_account(self, username, password):
         if username == manager["username"] and password == manager["password"]:
             customer = Customer()
             account_number = input("Enter the customer account to be deleted: ")
@@ -276,7 +290,7 @@ class Manager:
             print("Incorrect login credentials")
 
     # Method for manager to view customers details
-    def view_customers(username, password):
+    def view_customers(self, username, password):
         if username == manager["username"] and password == manager["password"]:
             customer = Customer()
             account_number = input("Enter the customer account to be deleted: ")
@@ -290,7 +304,7 @@ class MainApp:
         pass
     def appMenu(self):
         print("Wellcome to Memic Bank App")
-        print("Whatdo you want to perform?")
+        print("What do you want to perform?")
         print("1. Create Account for Customer as a staff.")
         print("2. Deposit Money as a Staff.")
         print("3. Withdraw Money as a Staff.")
@@ -302,22 +316,24 @@ class MainApp:
         print("9. Restrict Account Access as a Staff.")
         print("10. Restrict Account Access as a Bank Manager.")
         print("11. Delete Customer Account as a Bank Manager.")
+        print("12. Add staff as a Manager.")
 
     def startApp(self):
         self.appMenu()
         staffObj = Staff()
         customerObj = Customer()
+        managerObj = Manager()
         while True:
-            choice = input("Enter a number between 1 and 11: ")
+            choice = input("Enter a number between 1 and 12: ")
             match choice:
                 case "1":
                     staffId = input("Enter your Staff ID: ")
                     staffPassword = input("Enter your Password: ")
                     staffObj.createAccountForCustomer(staffId, staffPassword)
                 case "2":
-                    #staffId = input("Enter your Staff ID: ")
-                    #staffPassword = input("Enter your Password: ")
-                    pass
+                    staff_id = input("Enter your Staff ID: ")
+                    password = input("Enter your Password: ")
+                    staffObj.depositForCustomer(staff_id, password)
                 case "3":
                     staffId = input("Enter your Staff ID: ")
                     staffPassword = input("Enter your Log in Password: ")
@@ -349,4 +365,33 @@ class MainApp:
                             phone = input("Enter your  new Phone Number: ")
                             customerObj.updateDetails({"email" : email}, {"phone_number" : phone})
                 case "7":
-                    pass
+                    staffId = input("Enter your Staff ID: ")
+                    staffPassword = input("Enter your Log in Password: ")
+                    staffObj.previewDetails(staffId, staffPassword)
+                case "8":
+                    username = input("Enter login username: ")
+                    password = input("Enter your Log in Password: ")
+                    managerObj.view_customers(username, password)
+                case "9":
+                    staffId = input("Enter your Staff ID: ")
+                    staffPassword = input("Enter your Log in Password: ")
+                    staffObj.restrict_account_access(staffId, staffPassword)
+                case "10":
+                    username = input("Enter login username: ")
+                    password = input("Enter your Log in Password: ")
+                    managerObj.restrict_customer_access(username, password)
+                case "11":
+                    username = input("Enter login username: ")
+                    password = input("Enter your Log in Password: ")
+                    managerObj.delete_customer_account(username, password)
+                case "12":
+                    username = input("Enter login username: ")
+                    password = input("Enter your Log in Password: ")
+                    managerObj.createStaff(username, password)
+
+# To start the application
+# 1. craete instance of MainApp class:
+App = MainApp()
+
+# 2. Invoke the startApp method to begin
+App.startApp()
